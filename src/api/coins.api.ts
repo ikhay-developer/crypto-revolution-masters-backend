bf91670e234a6669a798b991c3366b46ecc2266f
daniel-rif <danielonyenwee@gmail.com>
2022-02-16 23:00:25 +0100

    🔁 refactor api to response the n number of coins data from m pages

--- src/api/coins.api.ts
+++ src/api/coins.api.ts
@@ -3,45 +3,55 @@ import { Router } from "express"
 
 const coinsApi:Router = Router()
 
-coinsApi.get("/", async (_, res) => {
-    try {
-        const coingeckoApi =  await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=4&sparkline=false&price_change_percentage=1h")
-        if (coingeckoApi.status >= 200 && coingeckoApi.status <= 300) {
-            let data = (coingeckoApi.data as Array<any>)
-            .map(({ 
-                name, 
-                symbol, 
-                image, 
-                current_price, 
-                market_cap, 
-                market_cap_rank, 
-                fully_diluted_valuation, 
-                market_cap_change_24h,
-                market_cap_change_percentage_24h,
-                ath_change_percentage,
-                high_24h, 
-                low_24h  
-            }) => ({
-                name, 
-                symbol, 
-                image, 
-                current_price, 
-                market_cap, 
-                market_cap_rank, 
-                fully_diluted_valuation,
-                market_cap_change_24h,
-                market_cap_change_percentage_24h,
-                ath_change_percentage, 
-                high_24h, 
-                low_24h
-            }))
-            res.send({ state: "success", data })
-        } else {
-            res.json({ state: "failed", reason: "backend error" })
+const fetchCoinsFromCoingecko = async (pageList:Array<string>): Promise<Array<any>> => {
+    let returnData:Array<any> = Array()
+    for await (const page of pageList) {
+        try {
+            const coingeckoApi =  await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${page}&sparkline=false&price_change_percentage=1h`)
+            if (coingeckoApi.status >= 200 && coingeckoApi.status <= 300) {
+                let data = (coingeckoApi.data as Array<any>)
+                .map(({ 
+                    name, 
+                    symbol, 
+                    image, 
+                    current_price, 
+                    market_cap, 
+                    market_cap_rank, 
+                    fully_diluted_valuation, 
+                    market_cap_change_24h,
+                    market_cap_change_percentage_24h,
+                    ath_change_percentage,
+                    high_24h, 
+                    low_24h  
+                }) => ({
+                    name, 
+                    symbol, 
+                    image, 
+                    current_price, 
+                    market_cap, 
+                    market_cap_rank, 
+                    fully_diluted_valuation,
+                    market_cap_change_24h,
+                    market_cap_change_percentage_24h,
+                    ath_change_percentage, 
+                    high_24h, 
+                    low_24h
+                }))
+                returnData.push(...data)
+            } else {
+                throw new Error()
+            }
+        } catch (error) {
+            returnData.push()
         }
-    } catch (error) {
-        res.json({ state: "failed", reason: "backend error" })
     }
+    return returnData
+}
+
+coinsApi.get("/", async (_, res) => {
+    fetchCoinsFromCoingecko(["1", "2", "3", "4"])
+    .then(data => res.send({ state: "success", data, size: data.length }))
+    .catch(_ => res.json({ state: "failed", reason: "backend error" }))
 })
 
 export default coinsApi
