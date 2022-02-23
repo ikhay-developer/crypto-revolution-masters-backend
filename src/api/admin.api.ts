2a67b5d9952d59cb8516225e94f4209acf39047f
daniel-rif <danielonyenwee@gmail.com>
2022-02-23 11:32:41 +0100

    ➕ add apis deleting ads and posting and getting admin mesaage

--- src/api/admin.api.ts
+++ src/api/admin.api.ts
@@ -10,6 +10,33 @@ import { table } from "../services/database"
 
 const adminApi = Router()
 
+adminApi.get("/message", async (_, res) => {
+    const snapshot = await getDoc(doc(table.admin, "message"))
+    if (snapshot.exists()) {
+        let data = Object(snapshot.data())
+        let dataArray = Array.from(Object.values(data))
+        res.json({ state: "success", data: dataArray })
+    } else {
+        res.json({ state: "failed", reason: "backend error" })
+    } 
+})
+
+adminApi.post("/message", async (req, res) => {
+    const snapshot = await getDoc(doc(table.admin, "message"))
+    const body = req.body
+    if (snapshot.exists()) {
+        let data = Object(snapshot.data())
+        let dataArray = Array.from(Object.values(data))
+        body["date"] = new Date().toISOString()
+        data[dataArray.length + 1] = body
+        setDoc(doc(table.admin, "message"), data)
+        .then(_ => res.json({ state: "success", data: {...body} }))
+        .catch(_ => res.json({ state: "failed", reason: "backend error" }))
+    } else {
+        res.json({ state: "failed", reason: "backend error" })
+    }
+})
+
 adminApi.post("/ads/:id", async (req, res) => {
     const snapshot = await getDoc(doc(table.admin, "ads"))
     const body = req.body
@@ -18,6 +45,19 @@ adminApi.post("/ads/:id", async (req, res) => {
         data[req.params.id] = {...body}
         setDoc(doc(table.admin, "ads"), data)
         .then(_ => res.json({ state: "success", data: {...body} }))
+        .catch(_ => res.json({ state: "failed", reason: "backend error" }))
+    } else {
+        res.json({ state: "failed", reason: "backend error" })
+    }
+})
+
+adminApi.delete("/ads/:id", async (req, res) => {
+    const snapshot = await getDoc(doc(table.admin, "ads"))
+    if (snapshot.exists()) {
+        let data = Object(snapshot.data())
+        delete data[req.params.id]
+        setDoc(doc(table.admin, "ads"), data)
+        .then(_ => res.json({ state: "success" }))
         .catch(_ => res.json({ state: "failed", reason: "backend error" }))
     } else {
         res.json({ state: "failed", reason: "backend error" })
