dcb33beaee8c3073d144e22c6ab5c991c1862891
daniel-rif <danielonyenwee@gmail.com>
2022-02-25 01:05:43 +0100

    ➕ add api for deleting file(s) stored in firebase cloud storage

--- src/api/storage.api.ts
+++ src/api/storage.api.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getStream, ref } from "firebase/storage"
+import { getStream, ref, deleteObject } from "firebase/storage"
 import storage from "../services/storage"
 
 const storageApi:Router = Router()
@@ -12,6 +12,19 @@ storageApi.get("/:directory/:file", (req, res) => {
         res.statusCode = 400
         res.send(`<pre>Cannot GET ${req.method} /${req.url}</pre>`)
     }
+})
+
+storageApi.delete("/:directory/:file", (req, res) => {
+    let imgRef = ref(storage, `${req.params.directory}/${req.params.file}`)
+    deleteObject(imgRef)
+    .then(_ => res.json({ state: "success" }))
+    .catch(e => {
+        if (e.code == "storage/object-not-found")
+            res.json({ state: "success" })
+        else
+            res.json({ state: "failed", reason: "backend error" })
+    })
+        
 })
 
 export default storageApi
