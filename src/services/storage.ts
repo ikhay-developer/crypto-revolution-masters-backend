10e3cc2edd4e2cd3d586ed06a600c97ec0acd801
daniel-rif <danielonyenwee@gmail.com>
2022-02-26 12:24:34 +0100

    🔁 refactor 2️⃣ uploadFile function to upload file with the current time stamp

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -9,7 +9,7 @@ import firebaseApp from "."
 const storage = getStorage(firebaseApp)
 
 export async function uploadFile (fromUrl:string, fileName:string,  destination:string, data:Blob|Uint8Array|ArrayBuffer): Promise<string> {
-    let fileRef = await uploadBytes(ref(storage, `/${destination}/${fileName}`), data)
+    let fileRef = await uploadBytes(ref(storage, `/${destination}/${fileName}-${new Date().toISOString()}`), data)
     const { name } = fileRef.metadata
     return `${fromUrl}/${destination}/${fileName}`
 }
