bef5822571c6c2c67bf3bfbfd575708ac55752ef
daniel-rif <danielonyenwee@gmail.com>
2022-04-26 16:04:02 +0100

    🛠 port the storage api to supabase

--- src/api/storage.api.ts
+++ src/api/storage.api.ts
@@ -1,38 +1,26 @@
+import { createClient } from "@supabase/supabase-js"
 import { Router } from "express"
-import { getStream, ref, deleteObject, getDownloadURL } from "firebase/storage"
-import storage from "../services/storage"
 
 const storageApi:Router = Router()
 
-storageApi.get("/:directory/:file", (req, res) => {
-    let imgRef = ref(storage, `${req.params.directory}/${req.params.file}`)
-    try {
-        getStream(imgRef).pipe(res)
-    } catch (error) {
-        res.statusCode = 400
-        let errorPage = String('<!DOCTYPE html>')
-            .concat('<html lang="en">')
-            .concat('<head>')
-            .concat('<meta charset="utf-8">\n<title>Error</title>')
-            .concat('</head>')
-            .concat('<body>')
-            .concat(`<pre>\nCannot GET ${req.method} ${req.url}\n</pre>`)
-            .concat('</body>')
-        res.send(errorPage)
-    }
-})
+const {
+    SUPABASE_URL,
+    SUPABASE_KEY
+} = process.env as {[name:string]: string}
 
-storageApi.delete("/:directory/:file", (req, res) => {
-    let imgRef = ref(storage, `${req.params.directory}/${req.params.file}`)
-    deleteObject(imgRef)
-    .then(_ => res.json({ state: "success" }))
-    .catch(e => {
-        if (e.code == "storage/object-not-found")
-            res.json({ state: "success" })
-        else
-            res.json({ state: "failed", reason: "backend error" })
-    })
-        
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+storageApi.delete("/:directory/:file", async (req, res) => {
+    let directory = req.params.directory
+    let file = req.params.file
+    const { error } = await supabase
+        .storage
+        .from(directory)
+        .remove([file])
+    if (!error)
+        res.json({ state: "success" })
+    else
+        res.json({ state: "failed", reason: "backend error" })
 })
 
 export default storageApi
